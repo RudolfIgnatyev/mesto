dc4ac8637ab9ac5a7a66c64548f717a040f13d7c
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2020-05-11 14:53:30 +0300

    fix: исправить отступы, эффекты при наведении и js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Находим элементы в DOM
 const editButton = document.querySelector('.profile__edit-button');
 const editProfile = document.querySelector('.popup');
 const closeIcon = document.querySelector('.popup__close-icon');
@@ -8,26 +9,33 @@ const profileSubtitle = document.querySelector('.profile__subtitle');
 const nameInput = document.querySelector('.popup__field_el_name');
 const jobInput = document.querySelector('.popup__field_el_profession');
 
+// Функция показа попапа
 function showPopup() {
   editProfile.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  const newName = nameInput.value;
-  const newJob = jobInput.value;
-  profileTitle.innerText = newName;
-  profileSubtitle.innerText = newJob;
-}
-
+// Функция скрытия попапа
 function hidePopup() {
   editProfile.classList.remove('popup_opened');
-  nameInput.value = '';
-  jobInput.value = '';
 }
 
+// Обработчик «отправки» формы
+function formSubmitHandler(evt) {
+  // Отменяем стандартную отправку формы
+  evt.preventDefault();
+  // Получаем значение полей из свойства value
+  const newName = nameInput.value;
+  const newJob = jobInput.value;
+  // Вставляем новые значения
+  profileTitle.innerText = newName;
+  profileSubtitle.innerText = newJob;
+  // Вызываем функцию скрытия попапа
+  hidePopup();
+}
+
+// Прикрепляем обработчики к элементам
 editButton.addEventListener('click', showPopup);
-formElement.addEventListener('submit', formSubmitHandler);
 closeIcon.addEventListener('click', hidePopup);
+formElement.addEventListener('submit', formSubmitHandler);
