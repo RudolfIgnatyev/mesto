981063f651e1996ba0d09899028a96d8efd7bcb1
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2020-05-25 14:06:31 +0300

    refactor: поместить элемент карточки в тег template

--- js/script.js
+++ js/script.js
@@ -8,6 +8,35 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const nameInput = document.querySelector('.popup__field_el_name');
 const jobInput = document.querySelector('.popup__field_el_profession');
+const cardsList = document.querySelector('.cards-list');
+const cardTemplate = document.querySelector('#cards-list__item-template').content;
+// Определяем базовый массив карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 // Функция показа попапа
 function showPopup() {
@@ -34,6 +63,21 @@ function formSubmitHandler(evt) {
   // Вызываем функцию скрытия попапа
   hidePopup();
 }
+
+// Функция добавления новой карточки
+function addCard(name, link) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.cards-list__title').textContent = name;
+  cardElement.querySelector('.cards-list__image').src = link;
+  cardElement.querySelector('.cards-list__image').alt = name;
+  cardsList.append(cardElement);
+}
+
+// Перебирем элементы массива базовых карточек
+initialCards.forEach(function (item) {
+  addCard(item.name, item.link);
+});
 
 // Прикрепляем обработчики к элементам
 editButton.addEventListener('click', showPopup);
